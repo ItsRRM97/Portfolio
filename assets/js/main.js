8ef8c941629e59bf2060f16d115c2ae77afe5c82
ItsRRM97 <mishra.roshanraj@gmail.com>
2018-09-24 22:03:51 +0530

    work page

--- assets/js/main.js
+++ assets/js/main.js
@@ -362,6 +362,100 @@
 
 	});
 
+	// Gallery image hover
+$( ".img-wrapper" ).hover(
+	function() {
+	  $(this).find(".img-overlay").animate({opacity: 1}, 600);
+	}, function() {
+	  $(this).find(".img-overlay").animate({opacity: 0}, 600);
+	}
+  );
+  
+  // Lightbox
+  var $overlay = $('<div id="overlay"></div>');
+  var $image = $("<img>");
+  var $prevButton = $('<div id="prevButton"><i class="fa fa-chevron-left"></i></div>');
+  var $nextButton = $('<div id="nextButton"><i class="fa fa-chevron-right"></i></div>');
+  var $exitButton = $('<div id="exitButton"><i class="fa fa-times"></i></div>');
+  var $launchButton = $('<a id="launchButton" class="button icon fa-download">Visit Website</a>');
+  
+  // Add overlay
+  $overlay.append($image).prepend($prevButton).append($nextButton).append($exitButton).append($launchButton);
+  $("#gallery").append($overlay);
+  
+  // Hide overlay on default
+  $overlay.hide();
+  
+  // When an image is clicked
+  $(".img-overlay").click(function(event) {
+	// Prevents default behavior
+	event.preventDefault();
+	// Adds href attribute to variable
+	var imageLocation = $(this).prev().attr("href");
+	// Add the image src to $image
+	$image.attr("src", imageLocation);
+	// Fade in the overlay
+	$overlay.fadeIn("slow");
+  });
+  
+  // When the overlay is clicked
+  $overlay.click(function() {
+	// Fade out the overlay
+	$(this).fadeOut("slow");
+  });
+  
+  // When next button is clicked
+  $nextButton.click(function(event) {
+	// Hide the current image
+	$("#overlay img").hide();
+	// Overlay image location
+	var $currentImgSrc = $("#overlay img").attr("src");
+	// Image with matching location of the overlay image
+	var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
+	// Finds the next image
+	var $nextImg = $($currentImg.closest(".image").next().find("img"));
+	// All of the images in the gallery
+	var $images = $("#image-gallery img");
+	// If there is a next image
+	if ($nextImg.length > 0) { 
+	  // Fade in the next image
+	  $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
+	} else {
+	  // Otherwise fade in the first image
+	  $("#overlay img").attr("src", $($images[0]).attr("src")).fadeIn(800);
+	}
+	// Prevents overlay from being hidden
+	event.stopPropagation();
+  });
+  
+  // When previous button is clicked
+  $prevButton.click(function(event) {
+	// Hide the current image
+	$("#overlay img").hide();
+	// Overlay image location
+	var $currentImgSrc = $("#overlay img").attr("src");
+	// Image with matching location of the overlay image
+	var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
+	// Finds the next image
+	var $nextImg = $($currentImg.closest(".image").prev().find("img"));
+	// Fade in the next image
+	$("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
+	// Prevents overlay from being hidden
+	event.stopPropagation();
+  });
+  
+  // When the exit button is clicked
+  $exitButton.click(function() {
+	// Fade out the overlay
+	$("#overlay").fadeOut("slow");
+  });
+
+  $launchButton.click(function() {
+	var $currentUrl = $(a.projectUrl).attr("href");
+	console.log($currentUrl);
+	window.open($currentUrl, '_blank');
+  })
+
 })(jQuery);
 
 var pathEls = document.querySelectorAll('path');
@@ -378,73 +472,6 @@ for (var i = 0; i < pathEls.length; i++) {
 		direction: 'alternate',
 		easing: 'easeInOutSine',
 		autoplay: true
-	});
-
-	$('.grid').masonry({
-		itemSelector: '.grid-item',
-		columnWidth: '.grid-sizer',
-		percentPosition: true
-	});
-
-	// Modal with transition
-	$('.grid-item').click(function (event) {
-		// Check if not already open
-		if (!$(this).hasClass('item-opened')) {
-			// Values
-			var elWidth = $(this).outerWidth() / 2;
-			var elPosition = this.getBoundingClientRect();
-
-			// Store position
-			$(this).attr('data-coord-left', $(this).css('left'));
-			$(this).attr('data-coord-top', $(this).css('top'));
-
-			// Transition effect
-			$(this).css({
-				top: elPosition.top,
-				left: elPosition.left
-			}).delay(400).css({
-				top: '120px',
-				left: '10%',
-				zIndex: '99999',
-				// marginLeft:	'-20%'
-				// position: 'fixed'
-			}).addClass('item-opened');
-
-			$('.grid-alpha').fadeIn();
-
-			// Scroll to the top
-			$('html, body').animate({
-				scrollTop: $('.grid').offset().top
-			}, 650);
-			$('.grid').css('overflow', 'visible');
-		} else {
-			$('.grid').css('overflow', 'hidden');
-		}
-	});
-
-
-	// Close item Modal
-	$(document).on('click', function (e) {
-		if ($('.item-opened').length > 0) {
-			if (!$(e.target).closest('.grid-item').length && !$(e.target).hasClass('item-opened')) {
-				$('.grid-alpha').fadeOut(650);
-
-				$('.item-opened').css({
-					top: $('.item-opened').data('coord-top'),
-					left: $('.item-opened').data('coord-left'),
-					marginLeft: ''
-				});
-
-				$('html, body').animate({
-					scrollTop: ($('.grid').offset().top + parseFloat($('.item-opened').data('coord-top'))) - 30
-				}, 650);
-
-				setTimeout(function () {
-					$('.grid-item').css('z-index', '').removeClass('item-opened');
-				}, 350);
-				$('.grid').css('overflow', 'hidden');
-			}
-		}
 	});
 
 }
